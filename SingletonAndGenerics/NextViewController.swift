0ab393bf78c4766221c04a5456b257ccb51895e6
Yuta Fujii <yutayutakid21@gmail.com>
2020-02-25 17:53:24 +0900

    課題

---
+++ SingletonAndGenerics/NextViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NextViewController.swift
+//  SingletonAndGenerics
+//
+//  Created by Fujii Yuta on 2020/02/25.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SingletonAndGenerics/ViewController.swift
+++ SingletonAndGenerics/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    /*
+     課題:Singletonクラスを作成し、tapを押したときにNextViewControllerのtextViewへ入力した値を反映
+     
+    計算ボタンを押した際、ジェネリクス関数を最高2つの用いてラベル通りの計算をする
+     */
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func keisan(_ sender: Any) {
+        //Singletonを用いることによるNextViewControllerへ渡す処理
+    
+    }
+    
+    @IBAction func tap(_ sender: Any) {
+        
+        //値を渡す
+    
+    }
+    
 }
 
